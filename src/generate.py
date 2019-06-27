2ea1e2414c986e149ec290106957f3cb7f83b445
milan.mosse <milan.mosse@gmail.com>
2019-06-27 16:17:45 -0700

    Bug fixes, renaming variables for consistency/clarity

--- src/generate.py
+++ src/generate.py
@@ -10,27 +10,27 @@ arbitrary.)
 @version June 2019
 """
 import numpy as np
-import torch
-import torch.utils.data
+
+import pytorch
 
 SQR_DIM = 28
 BLK_SQR = np.zeros((SQR_DIM,SQR_DIM))
 
-C = "4 with gaussian"	# cause
-O = "4 with uniform"	# correlate
+C = 4					# cause
+O = 4					# correlate
 E = 3					# effect
 p = {"causal": .5, "C": .8, "O": .8, "cE": .9, "bE": .2}
 
-def generate_vec():
-	world = {key: np.random.binomial(1,p[key]) for key in p.keys()}
-	cf_world = generate_cf_vec(world)
-	nums, utt = reformat(world)
-	# return image(nums, utt) # todo: turn C/O into ints for processing
+def generate_worlds():
+	act_world = {key: np.random.binomial(1,p[key]) for key in p.keys()}
+	cf_world = generate_cf_world(act_world)
+	nums, utt = reformat(act_world)
+	act_img = img(nums, utt)
 
-def image(nums, utt):
+def img(nums, utt):
 	# fill in top left and bottom right corners
-	cause_img = img_from_pos(nums[0])
-	caused_img = img_from_pos(nums[-1])
+	init_img = img_from_pos(nums[0])	# cause or correlate
+	effect_img = img_from_pos(nums[-1])
 	
 	# add noise
 	noise = BLK_SQR
@@ -38,11 +38,11 @@ def image(nums, utt):
 		noise = np.random.normal((SQR_DIM,SQR_DIM))
 	elif ("and" in utt):
 		noise = np.random.uniform(0,255, (SQR_DIM,SQR_DIM))
-	cause_img = np.minimum(np.add(noise, cause_img), 255)
+	init_img = np.minimum(np.add(noise, init_img), 255)
 
 	# put all four corner images together
-	img = np.concatenate((np.concatenate((cause_img, BLK_SQR), axis=0),
-                          np.concatenate((BLK_SQR, caused_img), axis=0)),
+	img = np.concatenate((np.concatenate((init_img, BLK_SQR), axis=0),
+                          np.concatenate((BLK_SQR, effect_img), axis=0)),
                          axis=1)
 	return img
 
@@ -53,24 +53,23 @@ def img_from_mnist(digit):
 	loc = np.random.choice(np.where(mnist["labels"] == digit)[0])
 	return mnist["digits"][loc]
 """
-[pos1, pos2]
-[pos3, pos4]
+[pos1, blnk]
+[blnk, pos2]
 
 """
 def reformat(world):
 	if(world["causal"]):
-		pos1 = str(C) if world["C"] else ""
-		pos4 = str(E) if (world["C"] and world["cE"]) else ""
+		cause = str(C) if world["C"] else ""
+		effect = str(E) if (world["C"] and world["cE"]) else ""
 
-		nums = [pos1, "", "", pos4]
-		utt = pos1 + ((" causes " + pos4) if pos4 else "")
+		nums = [cause, effect]
+		utt = cause + ((" causes " + effect) if effect else "")
 	else:
-		return [O if world["O"] else "", "", "", E if world["bE"] else ""]
-		pos1 = str(O) if world["O"] else ""
-		pos4 = str(E) if world["bE"] else ""
+		corr = str(O) if world["O"] else ""
+		effect = str(E) if world["bE"] else ""
 
-		nums = [pos1, "", "", pos4]
-		utt = pos1 + (("and " + pos4) if pos4 else "")
+		nums = [corr, effect]
+		utt = corr + ((" and " + effect) if effect else "")
 	return nums, utt
 
 def flip_rv(actual, key, key_to_vary):
@@ -84,14 +83,16 @@ def flip_rv(actual, key, key_to_vary):
 Given an actual set of values for the random variables, flip exactly one of
 those .
 """
-def flip_rvs(actual, key_to_vary):
-	return {key: flip_rv(actual, key, key_to_vary) for key in actual}
+def flip_rvs(act_world, key_to_vary):
+	return {key: flip_rv(act_world, key, key_to_vary) for key in act_world}
 
 """
 Given an actual set of values for the random variables, generate a list of
 counterfactuals by flipping each of the random variables, one at a time
 """
-def generate_cf_vec(actual):
-	cfs = [flip_rvs(actual, key_to_vary) for key_to_vary in actual]
+def generate_cf_world(act_world):
+	cfs = [flip_rvs(act_world, key_to_vary) for key_to_vary in act_world]
 	cf_vecs = [reformat(w)[0] for w in cfs]
 	return cf_vecs
+
+generate_worlds()
