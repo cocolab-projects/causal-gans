e3eeb9034fea9f27e8bba9097760bd086b4368db
Erin Bennett <erindb@stanford.edu>
2019-06-27 14:04:51 -0700

    update generate with cfs, too

--- src/generate.py
+++ src/generate.py
@@ -13,9 +13,9 @@ import numpy as np
 
 square_size = 4
 
-C = 4
-O = 4
-E = 3
+C = "4c"
+O = "4o"
+E = "3"
 
 # params for image generation
 # C is cause; E is effect
@@ -23,11 +23,35 @@ p = {"causal": .5, "C": .8, "O": .8, "cE": .9, "bE": .2}
 
 def generate_vec():
 	result = {key:np.random.binomial(1,p[key]) for key in p.keys()}
+	cf_vecs = generate_cf_vec(result)
 	vec = vec_from_result(result)
-	return img
+	return vec, cf_vecs
 
 def vec_from_result(result):
 	if(result["causal"]):
 		return [C if result["C"] else "", "", "", E if result["cE"] else ""]
 	else:
 		return [O if result["O"] else "", "", "", E if result["bE"] else ""]
+
+def flip_rv(actual, key, key_to_vary):
+	a = actual[key]
+	if (key == key_to_vary):
+		return (not a)
+	else:
+		return a
+
+"""
+Given an actual set of values for the random variables, flip exactly one of
+those .
+"""
+def flip_rvs(actual, key_to_vary):
+	return {key: flip_rv(actual, key, key_to_vary) for key in actual}
+
+"""
+Given an actual set of values for the random variables, generate a list of
+counterfactuals by flipping each of the random variables, one at a time
+"""
+def generate_cf_vec(actual):
+	cfs = [flip_rvs(actual, key_to_vary) for key_to_vary in actual]
+	cf_vecs = [vec_from_result(result) for result in cfs]
+	return cf_vecs
