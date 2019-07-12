Fixes to preprocesssing/trying out new nonlinear fns on causal imgs

src/generate.py
@@ -5,10 +5,11 @@ generate.py
 @version June 2019
 """
 import os
+import copy
 import numpy as np
 import matplotlib.pyplot as plt
 
-from utils import (clamp_img, standardize_img, viewable_img)
+from utils import (clamp_img, standardize_img, viewable_img, MIN_COLOR, MAX_COLOR, MNIST_MIN_COLOR, MNIST_MAX_COLOR)
 
 import torch
 import torch.utils.data
@@ -22,11 +23,11 @@ NUM1 = 4
 NUM2 = 3
 
 p = {"causal": .5, "C": .8, "noC": .8, "cE": .6, "bE": .5}
-event_functions = { "causal": lambda x: np.power(x, 1.1), # x**2,
-                    "C": lambda x: np.power(x,1.2), # (x+1)**2,
-                    "noC": lambda x: x,
-                    "cE": lambda x: np.power(x,1.15), # (x+2)**2,
-                    "bE": lambda x: x
+event_functions = { "causal": lambda img: warp(img, 1, 1),
+                    "C": lambda img: warp(img, 1, 1),
+                    "noC": lambda img: img,
+                    "cE": lambda img: warp(img, 1, 1),
+                    "bE": lambda img: img
                     }
 TOTAL_NUM_WORLDS = len(p) + 1
 
@@ -34,9 +35,6 @@ CORNER_DIM = 32
 IMG_DIM = CORNER_DIM*2
 BLK_SQR = np.zeros((CORNER_DIM,CORNER_DIM))
 
-# why do we vary "causal"?
-# causal entity or process
-# to do: clean up handling of case where cf
 def generate_worlds(mnist, n=1, cf = False, transform=True):
 
     scenarios = [] # a scenario is an actual world and its cfs
@@ -67,30 +65,24 @@ def imgs_of_worlds(worlds, mnist):
     four_img, three_img = num_from_mnist(NUM1, mnist), num_from_mnist(NUM2, mnist)
     return [img_of_world(world, four_img, three_img) for world in worlds]
 
+# note: BLK_SQR is all 0's; assumes no preprocessing
 def img_of_world(world, four_img, three_img):
     nums, utt = reformat(world)[0], reformat(world)[1]
 
     # set numbers in corners if they're in the world
     top_left = four_img if nums[0] else BLK_SQR
     bottom_right = three_img if nums[1] else BLK_SQR
-
-    # TEMP
-    temp = top_left
-    save_image(torch.from_numpy(viewable_img(top_left)), "original.png")
-    for event in world:
-        temp = event_functions[event](top_left)
-    save_image(torch.from_numpy(viewable_img(temp)), "modified.png")
-    # end TEMP
     
-    # apply nonlinear transformations
+    # apply nonlinear transformations; top_left is in interval [0,1]
     for event in world:
         if (event): top_left = event_functions[event](top_left)
-    top_left = clamp_img(top_left, standardized=True)
+    top_left = clamp_img(top_left, MNIST_MIN_COLOR, MNIST_MAX_COLOR)
 
     # put all four corner images together
-    return np.concatenate((np.concatenate((top_left, BLK_SQR), axis=1),
+    img = np.concatenate((np.concatenate((top_left, BLK_SQR), axis=1),
                       np.concatenate((BLK_SQR, bottom_right), axis=1)),
                      axis=0)
+    return standardize_img(img, from_unit_interval=True)
 
 def reformat(world):
     if(world["causal"]):
@@ -151,12 +143,12 @@ def load_mnist(root):
 
     # TODO: only get relevant digit values.
     train_data = {
-        'digits': np.concatenate([standardize_img(img, from_unit_interval=True) for img,label in train_loader], axis = 0)[:,0,...],
-        'labels': np.concatenate([label.numpy() for img,label in train_loader], axis = 0),
+        'digits': np.concatenate([imgs.numpy()*MAX_COLOR for imgs,labels in train_loader], axis = 0)[:,0,...],
+        'labels': np.concatenate([labels.numpy() for imgs,labels in train_loader], axis = 0),
     }
     test_data = {
-        'digits': np.concatenate([standardize_img(img, from_unit_interval=True) for img, label in test_loader],axis = 0)[:,0,...],
-        'labels': np.concatenate([label.numpy() for img,label in train_loader], axis = 0),
+        'digits': np.concatenate([imgs.numpy()*MAX_COLOR for imgs, labels in test_loader],axis = 0)[:,0,...],
+        'labels': np.concatenate([labels.numpy() for imgs,labels in train_loader], axis = 0),
     }
 
     return train_data, test_data
@@ -169,14 +161,12 @@ def mnist_dir_setup(train):
     train_mnist, test_mnist = load_mnist(DATA_DIR)
     return train_mnist if train else test_mnist
 
-import copy
-
-A = 20, B = 30
-def warp(img):
+def warp(img, A, B):
     new_img = copy.deepcopy(img)
-    for (u,v), value in np.ndenumerate(img):
-        x = u + A *np.sin(2.0 * np.pi  * v / B)
-        new_img[x][y] = img[u][v]
+    for u in range(len(img)):
+        for v in range(len(img[0])):
+            x = int(u + A *np.sin(2.0 * np.pi  * v / B))
+            new_img[x][v] = img[u][v]
     return new_img
 
 if __name__ ==  "__main__":
@@ -196,9 +186,3 @@ if __name__ ==  "__main__":
     mnist= mnist_dir_setup(args.train)
 
     img = mnist["digits"][0]
-
-    save_image(torch.from_numpy(viewable_img(img)), "original.png")
-    #for key in event_functions: img = event_functions[key](img)
-    save_image(torch.from_numpy(viewable_img(img)), "modified.png")
-    breakpoint()
-    # generate_worlds(mnist, n=args.dataset_size, cf=True)

src/train.py
@@ -22,88 +22,7 @@ from utils import (LossTracker, AverageMeter, save_checkpoint, free_params, froz
 
 CLASSIFIER_LOSS_WT = 1.0
 
-def log_reg_loop(loader, model, mode, pbar=None):
-    loss_meter = AverageMeter()
-    
-    if (mode != "test"):
-        criterion = torch.nn.BCELoss()
-    else:
-        causal_loss_meter = AverageMeter()
-
-    for i, (imgs, labels) in enumerate(loader):
-        imgs,labels = imgs.to(device), labels.to(device).float()
-        outputs = model(imgs)
-
-        # if testing for accuracy, round outputs; else add dim to labels
-        if (mode == "test"):
-            outputs = np.rint(outputs.numpy().flatten())
-            labels = labels.numpy()
-        else:
-            labels = labels.unsqueeze(1)
-
-        # calculate loss
-        if (mode == "test"):
-            loss = (outputs == labels)
-            loss_amt = np.mean(loss)
-
-            causal_indices = np.where(labels == 1)
-            causal_loss = (outputs[causal_indices] == labels[causal_indices])
-            causal_loss_amt = np.mean(causal_loss)
-            causal_loss_meter.update(causal_loss_amt, len(causal_indices))
-        else:
-            loss = criterion(outputs,labels)
-            loss_amt = loss.item()
-
-        loss_meter.update(loss_amt, loader.batch_size)
-
-        if (mode == "train"):
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-    return loss_meter.avg, causal_loss_meter.avg
-
-def log_reg_run_epoch(loader, model, mode, epoch=0):
-    if (mode == "train"):
-        model.train()
-        avg_loss, _ = log_reg_loop(loader, model, mode)
-    else:
-        model.eval()
-        with torch.no_grad():
-            avg_loss, avg_causal_loss = log_reg_loop(loader, model, mode)
-
-    if (mode=="test"):
-            print('====> test accuracy: {}%'.format(to_percent(avg_loss)))
-            print('====> test accuracy on causal pics: {}%'.format(to_percent(avg_causal_loss)))
-    elif epoch % 20 == 0:
-            print('====> {} epoch {}\tloss: {:.4f}'.format(mode, epoch, avg_loss))
-    return avg_loss
-
-def load_checkpoint(folder='./', filename='model_best.pth.tar'):
-    checkpoint = torch.load(folder + filename)
-    epoch = checkpoint['epoch']
-    loss_tracker = checkpoint['loss_tracker']
-    classifier = checkpoint['classifier']
-    GAN = checkpoint['GAN']
-    return epoch, loss_tracker, classifier, GAN
-
-def get_loss_d(wass, discriminator, imgs, gen_imgs, valid, fake):
-    if (wass):
-        return -torch.mean(discriminator(imgs)) + torch.mean(discriminator(gen_imgs))
-    else:
-        real_loss = torch.nn.BCELoss(discriminator(imgs), valid)
-        fake_loss = torch.nn.BCELoss(discriminator(gen_imgs), fake)
-        return (real_loss + fake_loss) / 2
-
-def get_loss_g(wass, discriminator, gen_imgs, valid):
-    if (wass):
-        return -torch.mean(discriminator(gen_imgs))
-    else:
-        return torch.nn.BCELoss(discriminator(gen_imgs), valid)
-
-def clip_discriminator(discriminator, clip_value):
-    for p in discriminator.parameters():
-        p.data.clamp_(-clip_value,clip_value)
+# ARGUMENTS; CHECKPOINTS AND PROGRESS WHILE TRAINING
 
 def handle_args():
     import argparse
@@ -141,12 +60,147 @@ def handle_args():
                         help="number of training steps for discriminator per iter")
     return parser.parse_args()
 
+def load_checkpoint(folder='./', filename='model_best.pth.tar'):
+    checkpoint = torch.load(folder + filename)
+    epoch = checkpoint['epoch']
+    loss_tracker = checkpoint['loss_tracker']
+    classifier = checkpoint['classifier']
+    GAN = checkpoint['GAN']
+    return epoch, loss_tracker, classifier, GAN
+
 def record_progress(epoch, epochs, batch, num_batches, loss_tracker):
     losses = [kind + ": " + str(loss_tracker.loss_kinds[kind].avg) for kind in loss_tracker.kinds]
     np.savetxt("./losses.txt", losses)
     progress = "[Epoch %d/%d] [Batch %d/%d]. Losses (running avg): %s" % (epoch, epochs, batch, num_batches, str(losses))
     print(progress)
 
+# COMPUTING LOSS FOR GAN (DISCRIMINATOR AND GENERATOR)
+
+def get_loss_d(wass, discriminator, imgs, gen_imgs, valid, fake):
+    if (wass):
+        return -torch.mean(discriminator(imgs)) + torch.mean(discriminator(gen_imgs))
+    else:
+        real_loss = torch.nn.BCELoss(discriminator(imgs), valid)
+        fake_loss = torch.nn.BCELoss(discriminator(gen_imgs), fake)
+        return (real_loss + fake_loss) / 2
+
+def get_loss_g(wass, discriminator, gen_imgs, valid):
+    if (wass):
+        return -torch.mean(discriminator(gen_imgs))
+    else:
+        return torch.nn.BCELoss(discriminator(gen_imgs), valid)
+
+# CLAMPING DISCRIMINATOR for GAN
+
+def clip_discriminator(discriminator, clip_value):
+    for p in discriminator.parameters():
+        p.data.clamp_(-clip_value,clip_value)
+
+# LOGISTIC REGRESSION TRAINING
+
+# single pass over all data
+def log_reg_loop(loader, model, mode, optimizer, pbar=None):
+    loss_meter = AverageMeter()
+    loss_tracker = LossTracker()
+    
+    if (mode != "test"):
+        criterion_log_reg = torch.nn.BCELoss()
+    else:
+        causal_loss_meter = AverageMeter()
+
+    for i, (imgs, labels) in enumerate(loader):
+        imgs,labels = imgs.to(device), labels.to(device).float()
+        outputs = model(imgs)
+
+        # if testing for accuracy, round outputs; else add dim to labels
+        if (mode == "test"):
+            outputs = np.rint(outputs.numpy().flatten())
+            labels = labels.numpy()
+        else:
+            labels = labels.unsqueeze(1)
+
+        # calculate loss
+        if (mode == "test"):
+            loss = (outputs == labels)
+            loss_amt = np.mean(loss)
+
+            causal_indices = np.where(labels == 1)
+            causal_loss = (outputs[causal_indices] == labels[causal_indices])
+            causal_loss_amt = np.mean(causal_loss)
+            loss_tracker.update("causal_loss", causal_loss_amt, len(causal_indices))
+        else:
+            loss = criterion_log_reg(outputs,labels)
+            loss_amt = loss.item()
+
+        loss_tracker.update(mode + "_loss", loss_amt, loader.batch_size)
+
+        if (mode == "train"):
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        record_progress(epoch, args.epochs, i, len(train_loader), loss_tracker)
+
+    return loss_meter.avg, causal_loss_meter.avg
+
+# sets model appropriately, calls log_reg_loop, and reports results
+def log_reg_run_epoch(loader, model, mode, tracker=None, epoch=0, optimizer = None):
+    if (mode == "train"):
+        model.train()
+        assert(optimizer is not None)
+        avg_loss, _ = log_reg_loop(loader, model, mode, optimizer)
+    else:
+        model.eval()
+        with torch.no_grad():
+            avg_loss, avg_causal_loss = log_reg_loop(loader, model, mode, None)
+
+    if (mode=="test"):
+            print('====> test accuracy: {}%'.format(to_percent(avg_loss)))
+            print('====> test accuracy on causal pics: {}%'.format(to_percent(avg_causal_loss)))
+    elif epoch % 20 == 0:
+            print('====> {} epoch {}\tloss: {:.4f}'.format(mode, epoch, avg_loss))
+    return avg_loss
+
+# test log reg
+def test_log_reg_from_checkpoint(args, cf):
+    test_dataset = CausalMNIST(split='test',cf=cf)
+    test_loader = DataLoader(test_dataset, shuffle=True, batch_size=args.batch_size)
+
+    test_model = LogisticRegression(cf)
+    _,_,classifier,GAN = load_checkpoint(folder=args.out_dir)
+    test_model.load_state_dict(classifier)
+    log_reg_run_epoch(test_loader, test_model, "test")
+
+# train/test/validate log reg
+def log_reg(train_loader, args, cf):
+    model = LogisticRegression(cf).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, betas=(args.b1, args.b2))
+
+    valid_dataset = CausalMNIST(split="validate", cf=cf)
+    valid_loader = DataLoader(valid_dataset, shuffle=True, batch_size=args.batch_size)
+
+    tracker = LossTracker()
+
+    for epoch in range(int(args.epochs)):
+        train_loss = log_reg_run_epoch(train_loader, model, "train", tracker, epoch, optimizer)
+        validate_loss = log_reg_run_epoch(valid_loader, model, "validate", tracker, epoch)
+            
+        tracker.best_loss = min(validate_loss, tracker.best_loss)
+        tracker.update("train_loss_log_reg", epoch, train_loss.item())
+        tracker.update("validate_loss_log_reg", epoch, validate_loss.item())
+
+        save_checkpoint({
+            'epoch': epoch,
+            'classifier': model.state_dict(),
+            'GAN': 0,
+            'loss_tracker': tracker,
+            'cmd_line_args': args,
+            'seed': args.seed
+        }, tracker.best_loss == validate_loss, folder = args.out_dir)
+
+    test_log_reg_from_checkpoint(args, cf)
+
+# MAIN: TRAINS GANs
 if __name__ == "__main__":
     # args
     args = handle_args()
@@ -155,16 +209,20 @@ if __name__ == "__main__":
     torch.manual_seed(args.seed)
     np.random.seed(args.seed)
 
-    # train, validate, test
     cf = False
-    transform = False
-    wass = True
-    attach_classifier = True
+    transform = True
 
     train_dataset = CausalMNIST(split="train", cf=cf, transform=transform)
     train_loader = DataLoader(train_dataset, shuffle=True, batch_size=args.batch_size)
 
-    # GAN
+    # train linear model alone
+    log_reg(train_loader=train_loader, args=args, cf=cf)
+
+    """
+    # train GANs
+    wass = True
+    attach_classifier = True
+
     generator = Generator(latent_dim=args.latent_dim, cf=cf, wass=wass)
     discriminator = Discriminator(cf=cf, wass=wass)
     # optimizer is set by constructor depending on the value of wass
@@ -263,10 +321,5 @@ if __name__ == "__main__":
             }, loss_tracker.best_loss == validate_loss, folder = args.out_dir)
 
     # test
-    test_dataset = CausalMNIST(split='test',cf=cf)
-    test_loader = DataLoader(test_dataset, shuffle=True, batch_size=args.batch_size)
-
-    test_model = LogisticRegression(cf)
-    _,_,classifier,GAN = load_checkpoint(folder=args.out_dir)
-    test_model.load_state_dict(classifier)
-    log_reg_run_epoch(test_loader, test_model, "test")
+    test_log_reg_from_checkpoint(args, cf)
+    """

src/utils.py
@@ -6,24 +6,23 @@ import numpy as np
 
 MIN_COLOR = 0.0
 MAX_COLOR = 255.0
+MNIST_MIN_COLOR = 0.0
+MNIST_MAX_COLOR = 1.0
+PROCESSED_MIN_COLOR = -1.0
+PROCESSED_MAX_COLOR = 1.0
 
 EPSILON = 1e-3      # window around z to resample
 PRIOR_WEIGHT = .5   # weights for prior (as opposed to posterior) distribution
 
 # UTILS: PREPROCESSING
 
-def clamp_img(img, standardized = True):
+def clamp_img(img, minimum, maximum):
     if (type(img) is not np.ndarray):
         img = img.numpy()
 
-    if (standardized):
-        maximum, minimum = 1, -1
-    else:
-        maximum, minimum = MAX_COLOR, MIN_COLOR
-
     return np.maximum(np.minimum(img, maximum), minimum)
 
-def standardize_img(img, from_unit_interval = True):
+def standardize_img(img, from_unit_interval=False):
     img = copy.deepcopy(img)
     if (type(img) is not np.ndarray):
         img = img.numpy()
@@ -31,9 +30,10 @@ def standardize_img(img, from_unit_interval = True):
     if (not from_unit_interval):
         img /= MAX_COLOR
 
+    # scale image from [0,1] to [-1,1]
     img = (img * 2.0) - 1.0
 
-    return clamp_img(img, True)
+    return clamp_img(img, PROCESSED_MIN_COLOR, PROCESSED_MAX_COLOR)
 
 def viewable_img(img, from_unit_interval = False):
     img = copy.deepcopy(img)
@@ -43,7 +43,7 @@ def viewable_img(img, from_unit_interval = False):
     if (not from_unit_interval):
         img = (img + 1.0) / 2.0
 
-    return clamp_img(img * MAX_COLOR, False)
+    return clamp_img(img * MAX_COLOR, MIN_COLOR, MAX_COLOR)
 
 # UTILS: TRAINING
 
@@ -75,6 +75,7 @@ class LossTracker():
 
     def update(self, loss_kind="loss", epoch=0, val=0, n=1):
         if (loss_kind not in self.loss_kinds):
+            assert(epoch == 0)
             meter = AverageMeter()
             self.loss_kinds[loss_kind] = [meter]
 
@@ -99,12 +100,14 @@ def frozen_params(module):
 def to_percent(float):
     return np.around(float*100, decimals=2)
 
-# UNTILS: RESAMPLING WITH ALI
+# UTILS: RESAMPLING WITH ALI
 
 def nearby(sample, z):
     return z - EPSILON <= sample and sample <= z + EPSILON
 
-def normal_resample(mean=0, var=1):
+# resamples from given dist until it finds something that
+# isn't close to z
+def normal_resample(z, mean=0, var=1):
     cf = np.random.normal(mean, var, batch_size)
     for img in batch_size:
         while (nearby(cf[img], z)):
@@ -116,14 +119,14 @@ def resample(z, post_mean, post_var, sample_from):
     latent_dim = z.size(1)
 
     if (sample_from == prior):
-        return normal_resample()
+        return normal_resample(z)
     elif (sample_from == "post"):
-        return normal_resample(post_mean, post_var)
+        return normal_resample(z, post_mean, post_var)
     elif (sample_from == "mix"):
-        if (np.random.binomial(1,PRIOR_WEIGHT)):
-            return normal_resample()
+        if (np.random.binomial(z, 1, PRIOR_WEIGHT)):
+            return normal_resample(z)
         else:
-            return normal_resample(post_mean, post_var)
+            return normal_resample(z, post_mean, post_var)
 
 def latent_cfs(z, post_mean, post_var, sample_from = "prior"):
     assert( sample_from == "prior" or